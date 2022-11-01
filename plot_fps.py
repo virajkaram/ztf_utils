915617fbd2546c6e79d1cc3bd44710b1edea8abc
viraj21197 <karambelkarvraj21197@gmail.com>
2022-11-01 15:55:47 -0700

    refactor

--- plot_fps.py
+++ plot_fps.py
@@ -11,24 +11,6 @@ from astropy.table import Column
 import matplotlib
 import pandas as pd
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--ztf_file",type=str,help="ZTF lightcurve file")
-parser.add_argument("--pgir_file",type=str,help="PGIR lightcurve file")
-parser.add_argument("--atlas_file",type=str,help="ATLAS lightcurve file")
-parser.add_argument("--mag",action='store_true',default=False,help="make only magnitudes as well")
-parser.add_argument("--bin",action='store_true',default=False,help="make only magnitudes as well")
-parser.add_argument("--rbin",type=int,default=1,help="make only magnitudes as well")
-parser.add_argument("--gbin",type=int,default=1,help="make only magnitudes as well")
-parser.add_argument("--ibin",type=int,default=1,help="make only magnitudes as well")
-parser.add_argument("--cbin",type=int,default=1,help="make only magnitudes as well")
-parser.add_argument("--obin",type=int,default=1,help="make only magnitudes as well")
-parser.add_argument("--xlim",default=None,type=float,help="set xlim to axis")
-parser.add_argument("--ymin",default=18,type=float,help="set ylim to axis")
-parser.add_argument("--ymax",default=22,type=float,help="set ylim to axis")
-
-args = parser.parse_args()
-jd0 = Time(datetime.utcnow()).jd
-
 def init():
     matplotlib.rcParams['xtick.minor.size'] = 6
     matplotlib.rcParams['xtick.major.size'] = 6
@@ -43,7 +25,6 @@ def init():
     matplotlib.rcParams['ytick.direction']='in'
     matplotlib.rcParams['xtick.direction']='in'
 
-init()
 
 
 def plot_fluxes(d,c='red',l='ZTF-g',ymn=-200,ymx=200,magticks=True):
@@ -181,16 +162,22 @@ def plot_binned(d, binsize,c='red',l='ZTF-r',ymn=-50,ymx=100,magticks=True):
     return plt
 
 
-def plot_binned_mags_df(d,binsize,c='red',l='ZTF-r',ymn=18,ymx=22,magticks=True,jd0=jd0):
+def plot_binned_mags_df(d,binsize,c='red',l='ZTF-r',ymn=18,ymx=22,magticks=True, jd0=Time(datetime.utcnow()).jd,write=True):
     bdf = pd.DataFrame.from_records(d,columns=d.colnames)
     dbins = np.arange(int(bdf['jd,'].min()),int(bdf['jd,'].max()),binsize)
     bdf_binned = bdf.groupby(np.digitize(bdf['jd,'],dbins))
     bdf_bin_flux = bdf_binned.apply(wavg_df)
 
-    bdjd, bdf, bdfunc = bdf_bin_flux['bjd'], bdf_bin_flux['bflux_uJy'], bdf_bin_flux['bfluxunc_uJy']
-    snr = bdf/bdfunc
     snt = 3
     snu = 5
+
+    bdf_bin_flux['maglim'] = -2.5*np.log10(bdf_bin_flux['bfluxunc_uJy']*5*1e-6/3631)
+    if write:
+        bdf_bin_flux.to_csv(f'fps_lcs/binned_{l}.csv')
+
+    bdjd, bdf, bdfunc = bdf_bin_flux['bjd'], bdf_bin_flux['bflux_uJy'], bdf_bin_flux['bfluxunc_uJy']
+    snr = bdf/bdfunc
+    
     detmask = (snr>snt)
     limmask = np.invert(detmask)
 
@@ -230,144 +217,168 @@ def plot_binned_mags(d,binsize,c='red',l='ZTF-r',ymn=18,ymx=22,magticks=True):
     return plt
 
 
-if args.ztf_file is None and args.pgir_file is None and args.atlas_file is None:
-	print('Please enter at least one lightcurve file')
-	exit(0)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--ztf_file",type=str,help="ZTF lightcurve file")
+    parser.add_argument("--pgir_file",type=str,help="PGIR lightcurve file")
+    parser.add_argument("--atlas_file",type=str,help="ATLAS lightcurve file")
+    parser.add_argument("--mag",action='store_true',default=False,help="make only magnitudes as well")
+    parser.add_argument("--bin",action='store_true',default=False,help="make only magnitudes as well")
+    parser.add_argument("--rbin",type=int,default=1,help="make only magnitudes as well")
+    parser.add_argument("--gbin",type=int,default=1,help="make only magnitudes as well")
+    parser.add_argument("--ibin",type=int,default=1,help="make only magnitudes as well")
+    parser.add_argument("--cbin",type=int,default=1,help="make only magnitudes as well")
+    parser.add_argument("--obin",type=int,default=1,help="make only magnitudes as well")
+    parser.add_argument("--xlim",default=None,type=float,help="set xlim to axis")
+    parser.add_argument("--ymin",default=18,type=float,help="set ylim to axis")
+    parser.add_argument("--ymax",default=22,type=float,help="set ylim to axis")
 
-
-fig,ax1 = plt.subplots(figsize=(10,7))
-if args.ztf_file is not None:
-    ztflc = ascii.read(args.ztf_file,format='basic')
-    print('Read %s'%(len(ztflc)))
-
-    try:
-        np.array(ztflc['forcediffimflux,'],dtype=float)
-    except ValueError:
-        ztflc = ztflc[ztflc['forcediffimflux,']!='null']
-
-    ztflc = ztflc[(ztflc['infobitssci,']==0) & (ztflc['scisigpix,']<25) & (ztflc['sciinpseeing,']<4)]
-    print('Found %s good points.'%(len(ztflc)))
-
-    ztflc.add_column(Column(name = 'forcediffimflux_uJy',data=np.array(ztflc['forcediffimflux,'],dtype=float)*10**(-0.4*(np.array(ztflc['zpmaginpsci,'])+48.6))/1e-29))
-    ztflc.add_column(Column(name = 'forcediffimfluxunc_uJy',data=np.array(ztflc['forcediffimfluxunc,'],dtype=float)*10**(-0.4*(np.array(ztflc['zpmaginpsci,'])+48.6))/1e-29))
-
-    i = ztflc[ztflc['filter,']=='ZTF_i']
-    r = ztflc[ztflc['filter,']=='ZTF_r']
-    g = ztflc[ztflc['filter,']=='ZTF_g']
+    args = parser.parse_args()
     jd0 = Time(datetime.utcnow()).jd
-    
+    init()
 
-    if args.mag:
-        if not args.bin:
-            plt.figure(figsize=(10,7))
-            plt = plot_mags(r,c='red',l='ZTF-r',ymn=args.ymin,ymx=args.ymax)
-            plt = plot_mags(i,c='brown',l='ZTF-i',ymn=args.ymin,ymx=args.ymax)
-            plt = plot_mags(g,c='green',l='ZTF-g',ymn=args.ymin,ymx=args.ymax)
-            #plt.axvline(specjd-jd0,linestyle='--',c='black')
-            objname = args.ztf_file.split('.dat')[0]
-            plt.legend(fontsize=12)
-            plt.xlabel('JD - %i [today]'%(jd0))
-            plt.ylabel('mag')
-            if args.xlim is not None:
-                plt.xlim(args.xlim,1)
 
-            if args.atlas_file is not None:
-                print('In Atlas')
-                atlaslcfile = args.atlas_file
-                plt = plot_mags_atlas(atlaslcfile,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
-                
-            plt.savefig('%s_mags.pdf'%(objname),bbox_inches='tight')
+    if args.ztf_file is None and args.pgir_file is None and args.atlas_file is None:
+    	print('Please enter at least one lightcurve file')
+    	exit(0)
+
+
+    fig,ax1 = plt.subplots(figsize=(10,7))
+    if args.ztf_file is not None:
+        ztflc = ascii.read(args.ztf_file,format='basic')
+        print('Read %s'%(len(ztflc)))
+
+        try:
+            np.array(ztflc['forcediffimflux,'],dtype=float)
+        except ValueError:
+            ztflc = ztflc[ztflc['forcediffimflux,']!='null']
+
+        ztflc = ztflc[(ztflc['infobitssci,']==0) & (ztflc['scisigpix,']<25) & (ztflc['sciinpseeing,']<4)]
+        print('Found %s good points.'%(len(ztflc)))
+
+        ztflc.add_column(Column(name = 'forcediffimflux_uJy',data=np.array(ztflc['forcediffimflux,'],dtype=float)*10**(-0.4*(np.array(ztflc['zpmaginpsci,'])+48.6))/1e-29))
+        ztflc.add_column(Column(name = 'forcediffimfluxunc_uJy',data=np.array(ztflc['forcediffimfluxunc,'],dtype=float)*10**(-0.4*(np.array(ztflc['zpmaginpsci,'])+48.6))/1e-29))
+
+        i = ztflc[ztflc['filter,']=='ZTF_i']
+        r = ztflc[ztflc['filter,']=='ZTF_r']
+        g = ztflc[ztflc['filter,']=='ZTF_g']
+        jd0 = Time(datetime.utcnow()).jd
+        
+
+        if args.mag:
+            if not args.bin:
+                plt.figure(figsize=(10,7))
+                plt = plot_mags(r,c='red',l='ZTF-r',ymn=args.ymin,ymx=args.ymax)
+                plt = plot_mags(i,c='brown',l='ZTF-i',ymn=args.ymin,ymx=args.ymax)
+                plt = plot_mags(g,c='green',l='ZTF-g',ymn=args.ymin,ymx=args.ymax)
+                #plt.axvline(specjd-jd0,linestyle='--',c='black')
+                objname = args.ztf_file.split('.dat')[0]
+                plt.legend(fontsize=12)
+                plt.xlabel('JD - %i [today]'%(jd0))
+                plt.ylabel('mag')
+                if args.xlim is not None:
+                    plt.xlim(args.xlim,1)
+
+                if args.atlas_file is not None:
+                    print('In Atlas')
+                    atlaslcfile = args.atlas_file
+                    plt = plot_mags_atlas(atlaslcfile,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+                    
+                plt.savefig('%s_mags.pdf'%(objname),bbox_inches='tight')
+
+            if args.bin:
+                plt.figure(figsize=(10,7))
+                if len(r)>0:
+                    plt = plot_binned_mags_df(r,args.rbin,c='red',l='ZTF-r',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+                if len(i)>0:
+                    plt = plot_binned_mags_df(i,args.ibin,c='brown',l='ZTF-i',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+                if len(g)>0:
+                    plt = plot_binned_mags_df(g,args.gbin,c='green',l='ZTF-g',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+                #plt.axvline(specjd-jd0,linestyle='--',c='black')
+                objname = args.ztf_file.split('.dat')[0]
+                plt.legend(fontsize=12)
+                plt.xlabel('JD - %i [today]'%(jd0))
+                plt.ylabel('mag')
+                if args.xlim is not None:
+                    plt.xlim(args.xlim,1)
+
+                if args.atlas_file is not None:
+                    print('In Atlas bin')
+                    atlaslcfile = args.atlas_file
+                    #objname = args.atlas_file.split('.dat')[0]
+        
+                    atlaslc = ascii.read(atlaslcfile)
+                    atlaslc['jd,'] = atlaslc['##MJD'] + 2400000.5
+                    atlaslc['forcediffimflux_uJy'] = np.array(atlaslc['uJy'],dtype=float)
+                    atlaslc['forcediffimfluxunc_uJy'] = np.array(atlaslc['duJy'],dtype=float)
+        
+                    c = atlaslc[atlaslc['F']=='c']
+                    o = atlaslc[atlaslc['F']=='o']
+                    plt = plot_binned_mags_df(c,args.cbin,c='cyan',l='ATLAS-c',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+                    plt = plot_binned_mags_df(o,args.obin,c='orange',l='ATLAS-o',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+
+                plt.legend()
+                plt.savefig('%s_binned_mags.pdf'%(objname),bbox_inches='tight')
+
+
+        plt = plot_fluxes(r,c='red',l='ZTF-r',magticks=False)
+        plt = plot_fluxes(i,c='brown',l='ZTF-i',magticks=False)
+        plt = plot_fluxes(g,c='green',l='ZTF-g',magticks=True)
+        objname = args.ztf_file.split('.dat')[0]
+        ax1.legend(fontsize=12)
 
         if args.bin:
-            plt.figure(figsize=(10,7))
-            plt = plot_binned_mags_df(r,args.rbin,c='red',l='ZTF-r',magticks=True,ymn=args.ymin,ymx=args.ymax)
-            plt = plot_binned_mags_df(i,args.ibin,c='brown',l='ZTF-i',magticks=True,ymn=args.ymin,ymx=args.ymax)
-            plt = plot_binned_mags_df(g,args.gbin,c='green',l='ZTF-g',magticks=True,ymn=args.ymin,ymx=args.ymax)
-            #plt.axvline(specjd-jd0,linestyle='--',c='black')
-            objname = args.ztf_file.split('.dat')[0]
-            plt.legend(fontsize=12)
-            plt.xlabel('JD - %i [today]'%(jd0))
-            plt.ylabel('mag')
-            if args.xlim is not None:
-                plt.xlim(args.xlim,1)
+            fig,ax1 = plt.subplots(figsize=(10,7))
+            plt = plot_binned(r,args.rbin,c='red',l='ZTF-r',magticks=False)
+            plt = plot_binned(g,args.gbin,c='green',l='ZTF-g',magticks=False)
+            plt = plot_binned(i,args.ibin,c='brown',l='ZTF-i',magticks=True)
+            ax1.legend(fontsize=12)
+            plt.savefig('%s_binned.pdf'%(objname),bbox_inches='tight')
 
-            if args.atlas_file is not None:
-                print('In Atlas bin')
-                atlaslcfile = args.atlas_file
-                #objname = args.atlas_file.split('.dat')[0]
-    
-                atlaslc = ascii.read(atlaslcfile)
-                atlaslc['jd,'] = atlaslc['##MJD'] + 2400000.5
-                atlaslc['forcediffimflux_uJy'] = np.array(atlaslc['uJy'],dtype=float)
-                atlaslc['forcediffimfluxunc_uJy'] = np.array(atlaslc['duJy'],dtype=float)
-    
-                c = atlaslc[atlaslc['F']=='c']
-                o = atlaslc[atlaslc['F']=='o']
-                plt = plot_binned_mags_df(c,args.cbin,c='cyan',l='ATLAS-c',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
-                plt = plot_binned_mags_df(o,args.obin,c='orange',l='ATLAS-o',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
 
-            plt.legend()
+
+    if args.pgir_file is not None:
+        pgirlcfile = args.pgir_file
+        with open(pgirlcfile,'r') as json_file:
+            lc = json.load(json_file)
+
+        jd0 = Time(datetime.utcnow()).jd
+        for key in lc.keys():
+            plt.plot(lc[key]['jd']-jd0,lc[key]['psfflux'],'.',c='purple')
+            plt.errorbar(lc[key]['jd']-jd0,lc[key]['psfflux'],lc[key]['psfflux_unc'],fmt='.',c='purple',label='PGIR J')
+
+        objname = pgirlcfile.split('lc')[1].split('_')[1]
+
+
+    if args.atlas_file is not None:
+        atlaslcfile = args.atlas_file
+        objname = args.atlas_file.split('.dat')[0]
+
+        atlaslc = ascii.read(atlaslcfile)
+        atlaslc['jd,'] = atlaslc['##MJD'] + 2400000.5
+        atlaslc['forcediffimflux_uJy'] = np.array(atlaslc['uJy'],dtype=float)
+        atlaslc['forcediffimfluxunc_uJy'] = np.array(atlaslc['duJy'],dtype=float)
+
+        c = atlaslc[atlaslc['F']=='c']
+        o = atlaslc[atlaslc['F']=='o']
+
+        if not args.bin:
+            ax1.plot(c['##MJD']+2400000.5-jd0,c['uJy'],'.',c='cyan',label='ATLAS-c')
+            ax1.errorbar(c['##MJD']+2400000.5-jd0,c['uJy'],c['duJy'],c='cyan',linestyle='',fmt='.')
+
+            ax1.plot(o['##MJD']+2400000.5-jd0,o['uJy'],'.',c='pink',label='ATLAS-o')
+            ax1.errorbar(o['##MJD']+2400000.5-jd0,o['uJy'],o['duJy'],c='pink',linestyle='',fmt='.')
+
+            ax1.legend(fontsize=9)
+
+        if args.bin:
+            fig, ax1 = plt.subplots(figsize=(10,7))
+            plt = plot_binned_mags_df(c,args.cbin,c='cyan',l='ATLAS-c',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
+            plt = plot_binned_mags_df(o,args.obin,c='orange',l='ATLAS-o',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
             plt.savefig('%s_binned_mags.pdf'%(objname),bbox_inches='tight')
 
-
-    plt = plot_fluxes(r,c='red',l='ZTF-r',magticks=False)
-    plt = plot_fluxes(i,c='brown',l='ZTF-i',magticks=False)
-    plt = plot_fluxes(g,c='green',l='ZTF-g',magticks=True)
-    objname = args.ztf_file.split('.dat')[0]
-    ax1.legend(fontsize=12)
-
-    if args.bin:
-        fig,ax1 = plt.subplots(figsize=(10,7))
-        plt = plot_binned(r,args.rbin,c='red',l='ZTF-r',magticks=False)
-        plt = plot_binned(g,args.gbin,c='green',l='ZTF-g',magticks=False)
-        plt = plot_binned(i,args.ibin,c='brown',l='ZTF-i',magticks=True)
-        ax1.legend(fontsize=12)
-        plt.savefig('%s_binned.pdf'%(objname),bbox_inches='tight')
+    if args.xlim is not None:
+        plt.xlim(args.xlim,1)
 
 
-
-if args.pgir_file is not None:
-    pgirlcfile = args.pgir_file
-    with open(pgirlcfilefile,'r') as json_file:
-        lc = json.load(json_file)
-
-    jd0 = Time(datetime.utcnow()).jd
-    for key in lc.keys():
-        plt.plot(lc[key]['jd']-jd0,lc[key]['psfflux'],'.',c='purple')
-        plt.errorbar(lc[key]['jd']-jd0,lc[key]['psfflux'],lc[key]['psfflux_unc'],fmt='.',c='purple',label='PGIR J')
-
-    objname = pgirlcfile.split('lc')[1].split('_')[1]
-
-
-if args.atlas_file is not None:
-    atlaslcfile = args.atlas_file
-    objname = args.atlas_file.split('.dat')[0]
-
-    atlaslc = ascii.read(atlaslcfile)
-    atlaslc['jd,'] = atlaslc['##MJD'] + 2400000.5
-    atlaslc['forcediffimflux_uJy'] = np.array(atlaslc['uJy'],dtype=float)
-    atlaslc['forcediffimfluxunc_uJy'] = np.array(atlaslc['duJy'],dtype=float)
-
-    c = atlaslc[atlaslc['F']=='c']
-    o = atlaslc[atlaslc['F']=='o']
-
-    if not args.bin:
-        ax1.plot(c['##MJD']+2400000.5-jd0,c['uJy'],'.',c='cyan',label='ATLAS-c')
-        ax1.errorbar(c['##MJD']+2400000.5-jd0,c['uJy'],c['duJy'],c='cyan',linestyle='',fmt='.')
-
-        ax1.plot(o['##MJD']+2400000.5-jd0,o['uJy'],'.',c='pink',label='ATLAS-o')
-        ax1.errorbar(o['##MJD']+2400000.5-jd0,o['uJy'],o['duJy'],c='pink',linestyle='',fmt='.')
-
-        ax1.legend(fontsize=9)
-
-    if args.bin:
-        fig, ax1 = plt.subplots(figsize=(10,7))
-        plt = plot_binned_mags_df(c,args.cbin,c='cyan',l='ATLAS-c',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
-        plt = plot_binned_mags_df(o,args.obin,c='orange',l='ATLAS-o',magticks=True,ymn=args.ymin,ymx=args.ymax,jd0=jd0)
-        plt.savefig('%s_binned_mags.pdf'%(objname),bbox_inches='tight')
-
-if args.xlim is not None:
-    plt.xlim(args.xlim,1)
-
-
-plt.savefig('%s.pdf'%(objname),bbox_inches='tight')
+    plt.savefig('%s.pdf'%(objname),bbox_inches='tight')
