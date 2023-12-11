46af9659b3a2da927adf3283d1f830a1c89e8763
viraj21197 <karambelkarvraj21197@gmail.com>
2023-12-11 22:45:49 +0530

    plotdir

--- plot_batch_fps.py
+++ plot_batch_fps.py
@@ -10,24 +10,27 @@ if __name__ == '__main__':
     parser.add_argument('filename', type=str,
                         help='Name of file to perform bfp on', default=None)
     parser.add_argument('-lcdir', type=str,
-                        help='Name of file to perform bfp on', default='lcs')
+                        help='Directory with lcs', default='lcs')
+    parser.add_argument('-plotdir', type=str,
+                        help='Directory with plots', default='.')
     args = parser.parse_args()
 
     df = pd.read_csv(args.filename)
 
-    clu_sources = pd.read_csv('all_clu_sources_20230412_fps.csv')
-    clu_sources_crds = SkyCoord(ra=clu_sources['ra [deg]'],
-                                dec=clu_sources['dec [deg]'],
-                                unit=(u.deg, u.deg))
+    # clu_sources = pd.read_csv('all_clu_sources_20230412_fps.csv')
+    # clu_sources_crds = SkyCoord(ra=clu_sources['ra [deg]'],
+    #                             dec=clu_sources['dec [deg]'],
+    #                             unit=(u.deg, u.deg))
     for ind, row in df.iterrows():
         ra, dec = row['ra'], row['dec']
         reqid = row['reqId']
         lcfile = f"{args.lcdir}/batchfp_{reqid}_lc.txt"
 
-        crd = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
-        idx, d2d, d3d = crd.match_to_catalog_sky(clu_sources_crds)
-        ztf_name = clu_sources.iloc[idx]['id']
-        ztf_name_lc = f"named_lcs/{ztf_name}_fps.dat"
+        # crd = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
+        # idx, d2d, d3d = crd.match_to_catalog_sky(clu_sources_crds)
+        # ztf_name = clu_sources.iloc[idx]['id']
+        ztf_name = row['reqid']
+        ztf_name_lc = f"{args.plotdir}/named_lcs/{ztf_name}_fps.dat"
         shutil.copy(lcfile, ztf_name_lc)
         os.system(f"python plot_fps.py --ztf_file {ztf_name_lc} --no_mask --mag")
         os.system(f"python plot_fps.py --ztf_file {ztf_name_lc} --no_mask --bin "
