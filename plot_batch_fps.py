82874c836419a54c893d0d4b20e8319a6994c4de
viraj21197 <karambelkarvraj21197@gmail.com>
2023-12-12 12:25:48 +0530

    binned mags

--- plot_batch_fps.py
+++ plot_batch_fps.py
@@ -33,6 +33,6 @@ if __name__ == '__main__':
         ztf_name_lc = f"{args.plotdir}/named_lcs/{ztf_name}_fps.dat"
         shutil.copy(lcfile, ztf_name_lc)
         os.system(f"python plot_fps.py --ztf_file {ztf_name_lc} --mag")
-        os.system(f"python plot_fps.py --ztf_file {ztf_name_lc} --bin "
+        os.system(f"python plot_fps.py --ztf_file {ztf_name_lc} --bin --mag "
                   f"--rbin 3 --gbin 3 --ibin 3")
         os.system(f"")
